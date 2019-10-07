788825323a6f830b670fb6c549b98b4672577928
Daniel Del Río Figueira <ddelrio@4eixos.com>
2019-10-07 08:32:42 +0200

    Control de error con tipo error

--- main.go
+++ main.go
@@ -16,7 +16,9 @@ type portForward struct {
 	hostPort string
 }
 
-func getPodName(deploy string) (string, string) {
+// Gets pod name from the first pod on deployment array
+// Returns pod name and error output
+func getPodName(deploy string) (string, error) {
 
 	cmd := exec.Command("kubectl", "get", "pods", "--namespace", "default", "-l", fmt.Sprintf("app=%s", deploy), "-o", "jsonpath={.items[0].metadata.name}")
 	cmdOutput := &bytes.Buffer{}
@@ -24,23 +26,25 @@ func getPodName(deploy string) (string, string) {
 	err := cmd.Run()
 
 	if err != nil {
-		// os.Stderr.WriteString(err.Error())
 		cmd.Wait()
-		return string(cmdOutput.Bytes()), fmt.Sprintf("%s pod not found", deploy)
+		return string(cmdOutput.Bytes()), fmt.Errorf("%s pod not found", deploy)
 	}
 
 	cmd.Wait()
-	return string(cmdOutput.Bytes()), "0"
+	return string(cmdOutput.Bytes()), nil
 }
 
+// Start port-forward in a goroutine
 func startForward(deploy, hostPort, podPort string, wg *sync.WaitGroup) {
 
+	// Finish goroutine at the end of this function
 	defer wg.Done()
 
 	for {
 		podName, err := getPodName(deploy)
 
-		if err != "0" {
+		if err != nil {
+			// If pod name wasn't found break the loop and finishes goroutine
 			fmt.Println(err)
 			break
 
@@ -48,23 +52,15 @@ func startForward(deploy, hostPort, podPort string, wg *sync.WaitGroup) {
 
 		t := time.Now().Format("2006-01-02 15:04:05")
 		fmt.Printf("[%s] Forwarding %-12s port %3s to local port %4s [pod: %s]\n", t, deploy, podPort, hostPort, podName)
-		// fmt.Println(deploy)
-		// fmt.Println("================================================================")
-		// fmt.Println(podName)
 		cmd := exec.Command("kubectl", "port-forward", fmt.Sprintf("pods/%s", podName), fmt.Sprintf("%s:%s", hostPort, podPort))
-		// fmt.Println("================================================================")
-		// fmt.Println(cmd)
 		cmdOutput := &bytes.Buffer{}
 		cmd.Stdout = cmdOutput
-		execErr := cmd.Run()
-		// fmt.Println(err)
+		err = cmd.Run()
 
-		if execErr != nil {
-			os.Stderr.WriteString(execErr.Error())
+		if err != nil {
+			os.Stderr.WriteString(err.Error())
 			fmt.Println("")
 		}
-
-		// fmt.Print(string(cmdOutput.Bytes()))
 
 		cmd.Wait()
 
@@ -74,8 +70,8 @@ func startForward(deploy, hostPort, podPort string, wg *sync.WaitGroup) {
 }
 
 func main() {
-	var podList [6]portForward
 
+	var podList [6]portForward
 	podList[0] = portForward{name: "auth", podPort: "80", hostPort: "8080"}
 	podList[1] = portForward{name: "calibration", podPort: "80", hostPort: "8081"}
 	podList[2] = portForward{name: "realtime", podPort: "80", hostPort: "8082"}
@@ -87,7 +83,7 @@ func main() {
 	wg.Add(len(podList))
 
 	for idx, _ := range podList {
-		// fmt.Println(podList[idx])
+		// Initiates a goroutine for every port-forward
 		go startForward(podList[idx].name, podList[idx].hostPort, podList[idx].podPort, &wg)
 	}
 	wg.Wait()
